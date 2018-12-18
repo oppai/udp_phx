10240fb69399639f844e30a46d6b2696fd823c42
hiroaki.murayama <hotsoup.h@gmail.com>
2018-12-18 23:03:01 +0900

    config修正

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :udp_phx, UdpPhxWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "127.0.0.1", port: 80]
 
 # Do not print debug messages in production
 config :logger, level: :info
