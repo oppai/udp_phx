74624207190d9407a9e8d02c5c483f5ac9e573e9
hiroaki.murayama <hotsoup.h@gmail.com>
2018-12-18 23:24:50 +0900

    heroku only one port binding

--- lib/udp_phx/application.ex
+++ lib/udp_phx/application.ex
@@ -6,13 +6,15 @@ defmodule UdpPhx.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
+    port = System.get_env("PORT")
+
     # Define workers and child supervisors to be supervised
     children = [
       # Start the endpoint when the application starts
-      supervisor(UdpPhxWeb.Endpoint, []),
+      # supervisor(UdpPhxWeb.Endpoint, []),
       # Start your own worker by calling: UdpPhx.Worker.start_link(arg1, arg2, arg3)
       # worker(UdpPhx.Worker, [arg1, arg2, arg3]),
-      supervisor(UdpPhxUdp.Application, [8080])
+      supervisor(UdpPhxUdp.Application, [port])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
